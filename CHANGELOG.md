b686348e7dd526ad994c59b03cfb9d0fd2244a0e
Andrew Kane <andrew@ankane.org>
2023-02-13 12:26:09 -0800

    Version bump to 0.1.2 [skip ci]

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,4 @@
-## 0.1.2 (unreleased)
+## 0.1.2 (2023-02-13)
 
 - Updated Mapkick.js to 0.2.4
 
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as fh:
 
 setup(
     name='mapkick',
-    version='0.1.1',
+    version='0.1.2',
     description='Create beautiful JavaScript maps with one line of Python',
     long_description=long_description,
     long_description_content_type='text/markdown',
